13c765f0a7cb945f22542de4ef609db8f7e3f1d1
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-13 00:01:54 +0100

    Refactor FocusableButton to accept FocusState binding directly
    The `FocusableButton` structure has been refactored to accept a `FocusState<Focus?>.Binding` for its focus state rather than a `FocusState<Focus?>`. This change allows for a more straightforward usage pattern where the binding can be passed directly, simplifying the syntax needed to create a `FocusableButton`. The actual focus management within the button's functionality remains unaffected by this internal change, but the improved interface facilitates easier integration within SwiftUI views.

--- Sources/Bonzai/Buttons/FocusableButton.swift
+++ Sources/Bonzai/Buttons/FocusableButton.swift
@@ -1,21 +1,20 @@
 import SwiftUI
 
 public struct FocusableButton<Content, Focus>: View where Content: View, Focus: Hashable {
-  @FocusState private var focus: Focus?
-
   private let identity: () -> Focus
   private let variant: ZenButtonVariant
   private let action: () -> Void
   private let label: () -> Content
+  private var focus: FocusState<Focus?>.Binding
 
   public init(
-    _ focus: FocusState<Focus?>,
+    _ focus: FocusState<Focus?>.Binding,
     identity: @escaping @autoclosure () -> Focus,
     variant: ZenButtonVariant,
     action: @escaping () -> Void,
     label: @escaping () -> Content
   ) {
-    _focus = focus
+    self.focus = focus
     self.identity = identity
     self.variant = variant
     self.action = action
@@ -25,14 +24,14 @@ public struct FocusableButton<Content, Focus>: View where Content: View, Focus: 
   public var body: some View {
     ZStack {
       Button(action: action, label: label)
-        .focusable($focus, as: identity(), onFocus: { })
+        .focusable(focus, as: identity(), onFocus: { })
         .buttonStyle(variant)
         .onCommand(#selector(NSResponder.insertNewline(_:)), perform: {
           action()
         })
     }
     .focusable(true)
-    .focused($focus, equals: identity())
+    .focused(focus, equals: identity())
     .focusEffectDisabled_shim()
 
   }
