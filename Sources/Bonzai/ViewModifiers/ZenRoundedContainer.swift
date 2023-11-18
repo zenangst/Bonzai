c6eea3e588908f275f945965c348464876a3d32d
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-11-18 14:00:40 +0100

    Introduce margin parameter to ZenRoundedContainer
    Added a 'margin' parameter to the ZenRoundedContainer ViewModifier enabling further customization of the view's surrounding space. This adjustment allows for better control over the view's layout, making it possible to define both the padding inside the container and the margin outside of it, increasing layout flexibility and improving visual consistency when the view is used in various contexts.

--- Sources/Bonzai/ViewModifiers/ZenRoundedContainer.swift
+++ Sources/Bonzai/ViewModifiers/ZenRoundedContainer.swift
@@ -3,27 +3,30 @@ import SwiftUI
 struct ZenRoundedContainer: ViewModifier {
   private let cornerRadius: CGFloat
   private let padding: CGFloat
+  private let margin: CGFloat
 
-  init(cornerRadius: CGFloat, padding: CGFloat) {
+  init(cornerRadius: CGFloat, padding: CGFloat, margin: CGFloat) {
     self.cornerRadius = cornerRadius
     self.padding = padding
+    self.margin = margin
   }
 
   func body(content: Content) -> some View {
     content
+      .padding(padding)
       .background(Color(nsColor: .underPageBackgroundColor))
       .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
       .background(
         RoundedRectangle(cornerRadius: cornerRadius)
           .stroke(Color(nsColor: .controlColor), lineWidth: 1)
       )
-      .padding(padding)
+      .padding(margin)
   }
 }
 
 public extension View {
-  func roundedContainer(_ cornerRadius: CGFloat = 8, padding: CGFloat = 16) -> some View {
+  func roundedContainer(_ cornerRadius: CGFloat = 8, padding: CGFloat = 16, margin: CGFloat = 16) -> some View {
     self
-      .modifier(ZenRoundedContainer(cornerRadius: cornerRadius, padding: padding))
+      .modifier(ZenRoundedContainer(cornerRadius: cornerRadius, padding: padding, margin: margin))
   }
 }
