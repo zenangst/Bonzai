40abe16f17b33ddf51dff2371dc817eb9df6b862
Christoffer Winterkvist <christoffer@winterkvist.com>
2025-01-23 18:18:56 +0100

    Update the calm style

--- Sources/Bonzai/Views/ZenStyleBackgroundView.swift
+++ Sources/Bonzai/Views/ZenStyleBackgroundView.swift
@@ -14,15 +14,16 @@ struct ZenStyleBackgroundView: View {
                                  isHovered: isHovered,
                                  nsColor: nsColor),
         startPoint: .top, endPoint: .bottom))
+      .opacity(fillOpacity())
       .background(
         RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
           .stroke(strokeColor(), lineWidth: 1)
           .offset(y: 0.25)
+          .opacity(strokeOpacity())
       )
-      .opacity(opacity())
   }
 
-  private func opacity() -> CGFloat {
+  private func fillOpacity() -> CGFloat {
     if calm && isHovered == false {
       return 0
     }
@@ -31,11 +32,25 @@ struct ZenStyleBackgroundView: View {
                      : colorScheme == .light ? 0.7 : 0.3
   }
 
+  private func strokeOpacity() -> CGFloat {
+    if calm && isHovered == false {
+      return 0.4
+    }
+
+    return isHovered ? 1.0
+    : colorScheme == .light ? 0.7 : 0.3
+  }
+
   private func strokeColor() -> Color {
+    if calm && isHovered == false {
+      return Color(colorScheme == .dark ? .textColor : .textColor)
+        .opacity(0.1)
+    }
+
     if colorScheme == .dark {
-      Color(nsColor: .shadowColor).opacity(0.2)
+      return Color(nsColor: .shadowColor).opacity(0.2)
     } else {
-      Color(nsColor: .shadowColor).opacity(0.2)
+      return Color(nsColor: .shadowColor).opacity(0.2)
     }
   }
 }
